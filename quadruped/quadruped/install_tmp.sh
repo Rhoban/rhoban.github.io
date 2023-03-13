26309e31057f1eea442400e5fb402da601dfed08
Grégoire Passault <g.passault@gmail.com>
2023-03-13 17:44:22 +0100

    Sourcing py3 all the time

--- quadruped/quadruped/install_tmp.sh
+++ quadruped/quadruped/install_tmp.sh
@@ -4,4 +4,7 @@ if [ ! -d /tmp/py3 ]; then
 	virtualenv --python=/usr/bin/python3 /tmp/py3
 	source /tmp/py3/bin/activate
 	pip3 install numpy scipy pybullet matplotlib
+else
+	source /tmp/py3/bin/activate
 fi
+
