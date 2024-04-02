ac58840f1955906cbeb8f08a54ed8cf1b4677c2d
Grégoire Passault <g.passault@gmail.com>
2024-04-03 00:24:59 +0200

    Mlp details

--- reperes/tp_pantilt/mlp.py
+++ reperes/tp_pantilt/mlp.py
@@ -5,21 +5,17 @@ class MLP(th.nn.Module):
     def __init__(self, input_dimension: int, output_dimension: int):
         super().__init__()
 
-        layers = [
-            th.nn.Linear(input_dimension, 256),
+        self.net = th.nn.Sequential(
+            th.nn.Linear(input_dimension, 3),
             th.nn.ReLU(),
-            th.nn.Linear(256, 256),
+            th.nn.Linear(3, 3),
             th.nn.ReLU(),
-            th.nn.Linear(256, 256),
-            th.nn.ReLU(),
-            th.nn.Linear(256, output_dimension),
-        ]
-
-        self.net = th.nn.Sequential(*layers)
+            th.nn.Linear(3, output_dimension),
+        )
 
     def forward(self, x):
         return self.net(x)
-    
+
     def load(self):
         self.load_state_dict(th.load("weights"))
 
